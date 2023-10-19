a972ca5ed184795af4b8ef45ddfe57939fcda807
unknown <christianangon29@gmail.com>
2023-10-19 19:26:51 +0800

    clean router

--- src/router/index.js
+++ src/router/index.js
@@ -1,42 +1,29 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+/*
+ * @Description:
+ * @Author: Ian
+ * @Date: 2023-10-19 17:29:19
+ * @LastEditTime: 2023-10-19 19:26:23
+ * @LastEditors: Ian
+ */
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
-    {
-        path: '/',
-        name: 'Home',
-        component: Home
-    },
-    {
-        path: '/category',
-        name: 'Category',
-        component: () => import("@/views/Category.vue"),
-    },
-    {
-        path: '/detail',
-        name: 'Detail',
-        component: () => import("@/views/Detail.vue"),
-    },
-    {
-        path: '/authors',
-        name: 'Authors',
-        component: () => import("@/views/Authors.vue"),
-    },
-    {
-        path: '/login',
-        name: 'Login',
-        component: () => import("@/views/Login.vue"),
-    }
-]
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+];
 
 const router = new VueRouter({
-    routes,
-    scrollBehavior() {
-        document.getElementById('app').scrollIntoView();
-    }
-})
+  routes,
+  scrollBehavior() {
+    document.getElementById("app").scrollIntoView();
+  },
+});
 
-export default router
+export default router;
